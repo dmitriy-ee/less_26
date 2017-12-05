f2a6344dc64b9e27079f63a8909f54272b330938
dmitriy_ee <dmitriy_ee@hotmail.com>
2017-12-05 23:50:00 +0200

    as hash

--- app_0.rb
+++ app_0.rb
@@ -85,3 +85,15 @@ end
 get '/contact' do
 
 end
+
+get '/showusers' do
+  	db = get_db
+  	db.results_as_hash = true
+  	db.execute 'SELECT *
+  	FROM Users' do |row|
+  	print row ['username']
+  	print "\t-\t"
+  	puts row [datestamp]
+  	puts '=============='
+  	end
+end
